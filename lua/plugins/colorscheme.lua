93e0cfd344948cb316313cbfc9dac0f454f8ff74
aneeshie <aneeshdas556@gmail.com>
2026-01-02 20:44:46 +0530

    Add Tokyonight theme to colorscheme options
    - Added folke/tokyonight.nvim plugin with night style
    - Configured with italic comments and keywords
    - Theme will be available in theme switcher

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -149,4 +149,30 @@ return {
       -- vim.cmd.colorscheme("nord")
     end,
   },
+  -- Tokyo Night theme
+  {
+    "folke/tokyonight.nvim",
+    priority = 1000,
+    config = function()
+      require("tokyonight").setup({
+        style = "night", -- The theme comes in three styles: storm, moon, night
+        light_style = "day", -- The theme is used when the background is set to light
+        transparent = false, -- Enable this to disable setting the background color
+        terminal_colors = true, -- Configure the colors used when opening a `:terminal`
+        styles = {
+          comments = { italic = true },
+          keywords = { italic = true },
+          functions = {},
+          variables = {},
+          sidebars = "dark", -- style for sidebars, see below
+          floats = "dark", -- style for floating windows
+        },
+        sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows
+        day_brightness = 0.3, -- Adjusts the brightness of the colors of the Day style
+        hide_inactive_statusline = false, -- Enabling this option will hide inactive statuslines
+        dim_inactive = false, -- dims inactive windows
+        lualine_bold = false, -- When true, section headers in the lualine theme will be bold
+      })
+    end,
+  },
 }
