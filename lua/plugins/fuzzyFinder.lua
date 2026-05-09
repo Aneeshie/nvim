066f3bb47cd0725727fb650a8b17501dd18ff509
Aneeshie <aneeshdas556@gmail.com>
2026-05-09 16:27:14 +0530

    fix: configure telescope fzf-native extension properly

--- lua/plugins/fuzzyFinder.lua
+++ lua/plugins/fuzzyFinder.lua
@@ -31,9 +31,7 @@ return {
           height = 0.80,
           preview_cutoff = 120,
         },
-        file_sorter = require("telescope.sorters").get_fuzzy_file,
         file_ignore_patterns = { "node_modules/", ".git/", "target/", "build/", "dist/", ".cache/" },
-        generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
         path_display = { "truncate" },
         winblend = 0,
         border = {},
@@ -45,6 +43,14 @@ return {
         grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
         qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
       },
+      extensions = {
+        fzf = {
+          fuzzy = true,
+          override_generic_sorter = true,
+          override_file_sorter = true,
+          case_mode = "smart_case",
+        }
+      }
     })
 
     require("telescope").load_extension("fzf")
